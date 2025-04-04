a86a7915a64a88a8fbec4534165e6457c69e9dd1
PaulaoTI <sistemassiscobra@gmail.com>
2025-04-04 11:56:14 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,37 @@
+const form = document.getElementById("form-agendamento");
 
+const customAlertErro = document.getElementById('customAlert');
+const customAlertSucesso = document.getElementById('customAlert1');
 
-document.getElementById("form-agendamento").addEventListener("submit", function (e) {
+const closeAlertBtnErro = document.getElementById('closeAlertBtnErro');
+const closeAlertBtnSucesso = document.getElementById('closeAlertBtnSucesso');
+
+closeAlertBtnErro.addEventListener('click', () => {
+    customAlertErro.style.display = 'none';
+});
+
+closeAlertBtnSucesso.addEventListener('click', () => {
+    customAlertSucesso.style.display = 'none';
+});
+
+window.addEventListener('click', (event) => {
+    if (event.target === customAlertErro) {
+        customAlertErro.style.display = 'none';
+    }
+    if (event.target === customAlertSucesso) {
+        customAlertSucesso.style.display = 'none';
+    }
+});
+
+form.addEventListener("submit", function (e) {
     e.preventDefault();
-    const nome = document.getElementById("nome").value;
+
+    const nome = document.getElementById("nome").value.trim();
     const data = document.getElementById("data").value;
     const linguagem = document.getElementById('linguagem').value;
 
     if (!nome || !data || !linguagem) {
-        alert('Por favor, preencha todos os campos.');
+        customAlertErro.style.display = 'block';
         return;
     }
 
@@ -17,9 +41,11 @@ document.getElementById("form-agendamento").addEventListener("submit", function 
     const ano = dataObj.getFullYear();
     const dataFormatada = `${dia}/${mes}/${ano}`;
 
-    const mensagem = `Nome: ${nome} ,Data agendada: ${dataFormatada} ,Linguagem selecionada: ${linguagem}.`;
-
+    const mensagem = `Nome: ${nome}, Data agendada: ${dataFormatada}, Linguagem selecionada: ${linguagem}.`;
     const link = `https://wa.me/5562981369705?text=${encodeURIComponent(mensagem)}`;
-
     window.open(link, '_blank');
+
+    customAlertSucesso.style.display = 'block';
+
+    form.reset();
 });
